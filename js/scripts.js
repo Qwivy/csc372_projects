580582db3750e6fb6d1c4d1386b3fba8f59a0bf2
John Doe <guano@112.206.20.172.s.wireless.uri.edu>
2025-02-09 21:34:12 -0500

    fix commit

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+var title = document.getElementById("title");
+title.style.color = "blue"; // Change the text color to blue
+
+const productsLink = document.getElementById("products-link");
+const faqLink = document.getElementById("faq-link");
+
+
+
+productsLink.addEventListener("click", function (e) {
+    console.log("Products link clicked!");
+    e.preventDefault(); // ignore navigation
+    alert("This page is under construction!"); 
+});
+
+faqLink.addEventListener("click", function (e) {
+    e.preventDefault(); // ignore navigation
+    alert("This page is under construction!"); 
+});
